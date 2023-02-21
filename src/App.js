321c7143db84321081ac23a64b9b36947777b400
Eten-Ekata <ekataelishaeten@gmail.com>
2023-02-20 16:49:02 -0800

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import {Box} from '@mui/material'
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Footer from './components/Footer'
 const App = () => {
   return (
-    <div>Apppp</div>
+    <Box width='400px' sx={{width:{xl:'1488px'}, border: 1, borderColor: 'primary.main' }} mx='auto'>
+        <Navbar />
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/exercise:id' element={<ExerciseDetail/>} />
+        </Routes>
+        <Footer/>
+    </Box>
   )
 }
 
