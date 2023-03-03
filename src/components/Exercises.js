7b146fb548a798f65bef9aab9889720f9eb3a282
Eten-Ekata <ekataelishaeten@gmail.com>
2023-03-03 04:24:35 -0800

    pagination

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -7,13 +7,36 @@ import ExerciseCard from './ExerciseCard'
 const Exercises = () => {
   const{exercises, bodyParts}=useContext(FitnessContext)
   // console.log(bodyParts)
+  const[currentPage, setCurrentPage] = useState(1)
+  const exercisePerPage=9;
+  const paginate=(e, value)=>{
+    setCurrentPage(value);
+    window.scrollTo({top:1600, behavior:'smooth'})
+  }
+  const IndexOfLastExercise=currentPage * exercisePerPage;
+  const IndexOfFirstExercise=IndexOfLastExercise - exercisePerPage;
+  const currentExerises= exercises.slice(IndexOfFirstExercise, IndexOfLastExercise);
   return (
     <Box sx={{mt:{lg:'110px'}}} mt='50px' p='20px'>
       <Typography variant='h3' mb='46px'> Showing Results</Typography>
       <Stack direction='row' sx={{gap:{lg:'110px', xs:'50px'}}} flexWrap='wrap' justifyContent='center'>
-      {exercises.map((exercise, index)=>(
+      {currentExerises.map((exercise, index)=>(
         <ExerciseCard key={index} exercise={exercise}/>
 ))}
+      </Stack>
+      <Stack mt='100px' alignItems='center'>
+        {exercises.length>9 && (
+          <Pagination 
+          color='primary'
+          shape='rounded'
+          variant="outlined"
+          defaultPage={1}
+          count={Math.ceil(exercises.length / exercisePerPage)}
+          page={currentPage}
+          onChange={paginate}
+          size='large'
+           />
+        )}
       </Stack>
     </Box>
   )
